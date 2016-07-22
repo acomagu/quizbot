a88ac4d872f0b64c15962a98891e851605302a61
Yuki Ito <acomagu@gmail.com>
2016-07-23 05:25:42 +0900

    [add] say question

--- reply.go
+++ reply.go
@@ -3,6 +3,26 @@ package main
 import ()
 
 func reply(text string, userID string) error {
-	_, err := bot.SendText([]string{userID}, "Hello, "+text)
+	var err error
+	userIDs := []string{userID}
+
+	question := []string{
+		"もんだぃ。ゎたしゎなんさぃ?",
+		"1. 14さぃ",
+		"2. 24さぃ",
+		"3. 64さぃ",
+	}
+
+	err = sendTexts(userIDs, question)
 	return err
+}
+
+func sendTexts(userIDs []string, texts []string) error {
+	for _, text := range texts {
+		_, err := bot.SendText(userIDs, text)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
