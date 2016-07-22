0a8b073d55664ce2519961e423c886059c5e303f
Yuki Ito <acomagu@gmail.com>
2016-07-22 23:02:22 +0900

    Revert "refactoring"
    This reverts commit 9006a51d6449e4daae8e8243e68120b907dc8c85.

--- main.go
+++ main.go
@@ -8,71 +8,51 @@ import (
 	"github.com/line/line-bot-sdk-go/linebot"
 )
 
-var bot *linebot.Client
-
 func main() {
-	var err error
-	configProxy()
+	fmt.Println("Hello, world!")
+	fmt.Println("I'm in heroku now!")
 
-	bot, err = lineClient()
-	if err != nil {
-		fmt.Println(err)
-	}
+	os.Setenv("HTTP_PROXY", os.Getenv("FIXIE_URL"))
+	os.Setenv("HTTPS_PROXY", os.Getenv("FIXIE_URL"))
 
-	listen()
-}
-
-func listen() {
-	port := os.Getenv("PORT")
-	http.HandleFunc("/callback", handleRequest)
-	http.ListenAndServe(":"+port, nil)
-}
-
-func handleRequest(w http.ResponseWriter, req *http.Request) {
-	fmt.Println(req.Body)
-
-	received, err := bot.ParseRequest(req)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	for _, result := range received.Results {
-		content := result.Content()
-		if content != nil && content.IsMessage && content.ContentType == linebot.ContentTypeText {
-			text, err := content.TextContent()
-			if err != nil {
-				fmt.Println(err)
-				continue
-			}
-			fmt.Println(text.Text)
-
-			err = reply(text.Text, content.From)
-			if err != nil {
-				fmt.Println(err)
-			}
-		}
-	}
-}
-
-func reply(text string, userID string) error {
-	_, err := bot.SendText([]string{userID}, "Hello, "+text)
-	return err
-}
-
-func configProxy() {
-	fixieURL := os.Getenv("FIXIE_URL")
-	os.Setenv("HTTP_PROXY", fixieURL)
-	os.Setenv("HTTPS_PROXY", fixieURL)
-}
-
-func lineClient() (*linebot.Client, error) {
 	lineChannelID, err := strconv.Atoi(os.Getenv("LINE_CHANNEL_ID"))
 	if err != nil {
 		fmt.Println(err)
 	}
 	lineChannelSecret := os.Getenv("LINE_CHANNEL_SECRET")
 	lineMID := os.Getenv("LINE_MID")
+	port := os.Getenv("PORT")
+
 	bot, err := linebot.NewClient(int64(lineChannelID), lineChannelSecret, lineMID)
-	return bot, err
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	http.HandleFunc("/callback", func(w http.ResponseWriter, req *http.Request) {
+		fmt.Println(req.Body)
+
+		received, err := bot.ParseRequest(req)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		for _, result := range received.Results {
+			content := result.Content()
+			if content != nil && content.IsMessage && content.ContentType == linebot.ContentTypeText {
+				text, err := content.TextContent()
+				if err != nil {
+					fmt.Println(err)
+					continue
+				}
+				fmt.Println(text.Text)
+
+				_, err = bot.SendText([]string{content.From}, "Hello, "+text.Text)
+				if err != nil {
+					fmt.Println(err)
+				}
+			}
+		}
+	})
+	http.ListenAndServe(":"+port, nil)
 }
